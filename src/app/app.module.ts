7e5ef3787f199358844ea216a19e6b1603c98ba6
ronaldoronaldo <dataweiss@gmail.com>
2018-07-17 00:02:26 -0300

    removing post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { PostComponent } from './post/post.component';
 import { AdminComponent } from './admin/admin.component';
 import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
     AdminComponent,
     PostListComponent
   ],
